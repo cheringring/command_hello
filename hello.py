147752a7c5e40b69a4f78273a213a8931d913ddf
cheringring <gksmfthsu000@gmail.com>
2024-04-03 13:50:20 +0900

    conflict resolved GitHub

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 // For command line git tutorial
 // First: GitHub modification
 // Second: Local repository modification
+
 print("hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+
+>>>>>>> origin/master
